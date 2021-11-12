574fe0a7309a27609077e1f322574a887b354e47
VanKhanhVuong <vankhanhvuong98@gmail.com>
2021-11-12 18:10:30 +0700

    feat: commit setup project

--- QuizApp/QuizApp/Screens/Main/MainViewController.swift
+++ QuizApp/QuizApp/Screens/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  QuizApp
 //
 //  Created by Văn Khánh Vương on 12/11/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
